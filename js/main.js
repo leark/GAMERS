f1291d3c0992c2ea77e9b0d25454855198f38386
Sam Wahbeh <stwahbeh@gmail.com>
2015-12-08 11:11:32 -0800

    repeated left sidebar

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@ myApp.config(function($stateProvider) {
     $stateProvider
 	.state('home', {
 		url:'/',
-		templateUrl: 'templates/home.html',
+		templateUrl: 'templates/forum/home.html',
 		controller: 'HomeController',
 	})
 	// Configure states for "content" and "about"
 
 	.state('newPost', {
 		url: '/newPost',
-		templateUrl: 'templates/newPost.html',
+		templateUrl: 'templates/forum/newPost.html',
 		controller: 'NewPostController',
 	})
 }) 
@@ -22,35 +22,49 @@ myApp.config(function($stateProvider) {
 .controller('NewPostController', function($scope){
 })
 
-$('#titleleft').click(function() {
-	if($('#leftbar').css('display') == 'none') {
-		$('#leftbar').css('display', 'initial')
-		$('#left').css('height', 'auto')
-		$('#left').css('z-index', '1001')
-	} else {
-		$('#leftbar').css('display', 'none')
-		$('#left').css('z-index', '0')
-	}
-});
+// loads after page is done loading
+$(function() {
+	$('#login').click(function() {
+		var ref = new Firebase("https://gamersuw.firebaseio.com");
+		ref.authWithOAuthPopup("facebook", function(error, authData) {
+		  if (error) {
+		    console.log("Login Failed!", error);
+		  } else {
+		    console.log("Authenticated successfully with payload:", authData);
+		  }
+		});
+	})
 
-$('#titleright').click(function() {
-	if($('#rightbar').css('display') == 'none') {
-		$('#rightbar').css('display', 'initial')
-		$('#right').css('height', 'auto')
-		$('#right').css('position', 'absolute')
-		$('#right').css('z-index', '1001')
-	} else {
-		$('#rightbar').css('display', 'none')
-		$('#right').css('z-index', '0')
-	}
-});
+	$('#titleleft').click(function() {
+		if($('#leftbar').css('display') == 'none') {
+			$('#leftbar').css('display', 'initial')
+			$('#left').css('height', 'auto')
+			$('#left').css('z-index', '1001')
+		} else {
+			$('#leftbar').css('display', 'none')
+			$('#left').css('z-index', '0')
+		}
+	});
 
-$(window).resize(function() {
-	if($('#titleleft').css('display') == 'none') {
-		$('#rightbar').css('display', 'initial')
-		$('#leftbar').css('display', 'initial')
-	} else {
-		$('#rightbar').css('display', 'none')
-		$('#leftbar').css('display', 'none')
-	}
-});
+	$('#titleright').click(function() {
+		if($('#rightbar').css('display') == 'none') {
+			$('#rightbar').css('display', 'initial')
+			$('#right').css('height', 'auto')
+			$('#right').css('position', 'absolute')
+			$('#right').css('z-index', '1001')
+		} else {
+			$('#rightbar').css('display', 'none')
+			$('#right').css('z-index', '0')
+		}
+	});
+
+	$(window).resize(function() {
+		if($('#titleleft').css('display') == 'none') {
+			$('#rightbar').css('display', 'initial')
+			$('#leftbar').css('display', 'initial')
+		} else {
+			$('#rightbar').css('display', 'none')
+			$('#leftbar').css('display', 'none')
+		}
+	});
+})
