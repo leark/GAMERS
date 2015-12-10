5689d97317794cf7d557e3432a0474640637bf9e
Seung Lee <shl7@uw.edu>
2015-12-09 22:26:16 -0800

    firebase addition for threads

---
+++ templates/newThread.php
@@ -0,0 +1,6 @@
+<?php 
+
+echo "<a href='https://disqus.com/api/oauth/2.0/authorize/?client_id=zFYDrRp7UkXfhX3xWuGaLQfi2T0hBjUeJLAszIKIC0RObnKclNc1yPkDGslOotqB&scope=read,write&response_type=code&redirect_uri=http://localhost:8080/oauth-callback.php'>OAuth</a>";
+
+
+?>
